3406c1ad8c1b7d4ab1047e394d33b0faeba3f47d
S_KHALIFA <65005134+Psykali@users.noreply.github.com>
2025-06-03 15:30:11 +0200

    Delete install_requirement.sh

--- install_requirement.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-/home/psyko/.pyenv/versions/3.10.6/envs/BIMpredict/bin/python -m pip install -r requirements.txt && \
-/home/psyko/.pyenv/versions/3.10.6/envs/BIMpredict/bin/python -m pip install --upgrade pip
