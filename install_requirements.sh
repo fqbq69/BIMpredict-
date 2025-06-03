c5e37fb50bb4f9891dd7a6ec9c87cf2f6065a2b8
S_KHALIFA <65005134+Psykali@users.noreply.github.com>
2025-06-03 14:55:46 +0200

    updating requirments

---
+++ install_requirements.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+/home/psyko/.pyenv/versions/3.10.6/envs/BIMpredict/bin/python -m pip install -r requirements.txt && \
+/home/psyko/.pyenv/versions/3.10.6/envs/BIMpredict/bin/python -m pip install --upgrade pip
